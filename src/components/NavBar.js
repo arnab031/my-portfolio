1cfd7fb340483fa977a2ddc99446a661b872b307
arnab031 <51916075+arnab031@users.noreply.github.com>
2021-05-14 01:55:16 +0530

    navbar color changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,35 +4,35 @@ import { SocialIcon } from "react-social-icons";
 
 function NavBar() {
   return (
-    <header className="bg-red-600">
+    <header className="bg-green-800">
       <div className="container mx-auto flex justify-between">
         <nav className="flex">
           <NavLink
             to="/"
             exact
             activeClassName="text-white"
-            className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
+            className="inline-flex items-center py-6 px-3 mr-4 text-black-100 hover:text-green-200 text-4xl font-bold cursive tracking-widest"
           >
             Arnab
           </NavLink>
           <NavLink
             to="/post"
             activeClassName="text-red-100 bg-red-700"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-black-200 hover:text-green-200"
           >
             Blog Posts
           </NavLink>
           <NavLink
             to="/project"
             activeClassName="text-red-100 bg-red-700"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-black-200 hover:text-green-200"
           >
             Projects
           </NavLink>
           <NavLink
             to="/about"
             activeClassName="text-red-100 bg-red-700"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-black-200 hover:text-green-200"
           >
             About Me!
           </NavLink>
